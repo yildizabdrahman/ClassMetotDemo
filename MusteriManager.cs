c79ad0d46861e36b04f67f5b613f931cfcf99dbe
yildizabdrahman <yildizabdrahman@gmail.com>
2021-01-21 02:06:04 +0300

    selam yanlızlık ben geldim

---
+++ MusteriManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.Soyad);
+                Console.WriteLine(musteri.Tckn);
+                Console.WriteLine(musteri.DogumYili);
+                Console.WriteLine("\n");
+            }
+          
+
+        }
+
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" "+musteri.Soyad+" Eklendi");
+
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " Silindi");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Serhat";
+            musteri1.Soyad = "Yıldız";
+            musteri1.Tckn = "12345678";
+            musteri1.DogumYili = 1983;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Abdurrahman";
+            musteri2.Soyad = "Yıldız";
+            musteri2.Tckn = "12345678";
+            musteri2.DogumYili = 1983;
+
+            Musteri[] musteriList = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriListele(musteriList);
+            musteriManager.Add(musteri1);
+            musteriManager.Delete(musteri2);
+
+
+        }
+    }
+}
